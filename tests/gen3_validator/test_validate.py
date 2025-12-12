6f1100efa103a87c222272c797ea5cf21e9e60b4
JoshuaHarris391 <harjo391@gmail.com>
2025-12-12 14:55:54 +1100

    Refactor validation logic and add new validation functions
    - Replaced the Validate class with standalone functions for validating JSON objects and lists against a schema.
    - Introduced `validate_object`, `validate_list_dict`, and `pull_schema` functions to streamline validation processes.
    - Updated error handling to provide more informative messages when validation fails.
    - Added unit tests for new validation functions to ensure correctness and handle edge cases, including missing 'type' keys.

--- tests/gen3_validator/test_validate.py
+++ tests/gen3_validator/test_validate.py
@@ -2,6 +2,9 @@ import pytest
 import gen3_validator as gen3_validator
 from typing import List, Dict, Any
 import os
+from unittest.mock import patch
+
+
 
 @pytest.fixture
 def fixture_data_pass() -> List[Dict[str, Any]]:
@@ -82,3 +85,78 @@ def fixture_resolver_inst(fixture_schema_path) -> dict:
 
 def test_resolved_schema_version(fixture_resolver_inst):
     assert fixture_resolver_inst.get_schema_version() == "1.0.0"
+
+
+def test_validate_json_pass(fixture_resolver_inst):
+    data = [
+        {
+            "atrial_fibrillation": "yes",
+            "cabg": "yes",
+            "type": "medical_history"
+        }
+    ]
+
+    expected = [
+        {
+            'node': 'medical_history',
+            'index': 0,
+            'validation_result': 'PASS',
+            'invalid_key': None,
+            'schema_path': None,
+            'validator': None,
+            'validator_value': None,
+            'validation_error': None
+        }
+    ]
+    resolved_schema = fixture_resolver_inst.schema_resolved
+    result = gen3_validator.validate.validate_list_dict(data, resolved_schema)
+    assert result == expected
+
+
+def test_validate_json_fail(fixture_resolver_inst):
+    data = [
+        {
+            "atrial_fibrillation": 2,
+            "cabg": 14,
+            "type": "medical_history"
+        }
+    ]
+
+    expected = [
+        {
+            'node': 'medical_history',
+            'index': 0,
+            'validation_result': 'FAIL',
+            'invalid_key': 'atrial_fibrillation',
+            'schema_path': 'properties.atrial_fibrillation.enum',
+            'validator': 'enum',
+            'validator_value': ['yes', 'no'],
+            'validation_error': "2 is not one of ['yes', 'no']"
+        },
+        {
+            'node': 'medical_history',
+            'index': 0,
+            'validation_result': 'FAIL',
+            'invalid_key': 'cabg',
+            'schema_path': 'properties.cabg.enum',
+            'validator': 'enum',
+            'validator_value': ['yes', 'no'],
+            'validation_error': "14 is not one of ['yes', 'no']"
+        }
+    ]
+    resolved_schema = fixture_resolver_inst.schema_resolved
+    result = gen3_validator.validate.validate_list_dict(data, resolved_schema)
+    assert result == expected
+
+
+def test_missing_type_key(fixture_resolver_inst):
+    data = [
+        {
+            "atrial_fibrillation": "yes",
+            "cabg": "yes"
+        }
+    ]
+    resolved_schema = fixture_resolver_inst.schema_resolved
+
+    with pytest.raises(Exception, match="Error in validate_list_dict during object validation at index 0, key 'type' not found in"):
+        gen3_validator.validate.validate_list_dict(data, resolved_schema)
